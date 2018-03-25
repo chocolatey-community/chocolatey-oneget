da447511a632682b538257c860bda58b67dd5bac
JirkaPok <j.pok@post.cz>
2018-03-25 15:23:24 +0200

    Implemented psake build scripts

--- Build/prepareEnvironment.ps1
+++ Build/prepareEnvironment.ps1
@@ -5,7 +5,9 @@ Set-ExecutionPolicy Bypass -Scope Process;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 
 # Testing framework
-Install-Module -Name Pester -Force -SkipPublisherCheck;
+Install-Module -Name Pester -Force -SkipPublisherCheck -Scope "CurrentUser" -RequiredVersion 4.3.1;
 Import-Module -Name Pester;
+Install-Module -Name psake -Force -Scope "CurrentUser" -RequiredVersion 4.7.0;
+Import-Module -Name psake;
 
-choco install paket -y;
+choco install paket -y -version 5.133.0;
---
+++ Build/psakefile.ps1
@@ -0,0 +1,36 @@
+$targetFolder = Join-Path $PSScriptRoot "Output";
+$moduleFolder = Join-Path $targetFolder "Chocolatey-OneGet";
+
+Task Default -Depends `
+    Clean-Repository,`
+    Clean,`
+    Restore-Packages,`
+    Compile,`
+    Test;
+
+Task Restore-Packages {
+    Exec {
+        paket restore;
+    }
+}
+
+Task Clean {
+    Remove-Item $targetFolder -Force -Recurse -ErrorAction SilentlyContinue;
+}
+
+Task Compile {
+    md "$targetFolder\Chocolatey-OneGet" -ErrorAction SilentlyContinue | Out-Null;
+    copy -Path "..\Chocolatey-OneGet.psd1" -Destination "$moduleFolder\Chocolatey-OneGet.psd1" -Force;
+    copy -Path "..\Chocolatey-OneGet.psm1" -Destination "$moduleFolder\Chocolatey-OneGet.psm1" -Force;
+
+    Register-PSRepository -Name TargetRepo -SourceLocation $targetFolder -PublishLocation $targetFolder -InstallationPolicy Trusted | Out-Null;
+    Publish-Module -Path $moduleFolder -Repository TargetRepo -Force;
+}
+
+Task Clean-Repository {
+    Unregister-PSRepository TargetRepo -ErrorAction SilentlyContinue;
+}
+
+Task Test {
+    # TODO Run Pester tests
+}
