65f5458cbbab8d982cb5883b9e4900bed280477b
JirkaPok <j.pok@post.cz>
2018-03-25 23:30:36 +0200

    Implemented most of the module manifest

--- Tests/ModuleTests.ps1
+++ Tests/ModuleTests.ps1
@@ -2,6 +2,6 @@ Describe 'Chocolatey-OneGet Module API' {
     It "It installs" {
         Install-Module Chocolatey-OneGet -Repository TargetRepo -Force -Scope "CurrentUser";
         Import-Module Chocolatey-OneGet -Force;
-        Get-Module Chocolatey-OneGet | Should -Not -BeNullOrEmpty;
+        Get-Module Chocolatey-OneGet | Should -Not -Be $null;
     }
 }
