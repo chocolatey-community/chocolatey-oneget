4b79cda6f5f0ef07e3e6954a3e4a1e9ad61c1173
JirkaPok <j.pok@post.cz>
2018-04-19 22:00:10 +0200

    Added Helpers file

---
+++ Chocolatey-Helpers.psm1
@@ -0,0 +1,14 @@
+function IsChocolateyInstalled() {
+    $script:chocolateyDir = $null
+    if ($env:ChocolateyInstall -ne $null) {
+        $script:chocolateyDir = $env:ChocolateyInstall;
+    }
+    elseif (Test-Path (Join-Path $env:SYSTEMDRIVE Chocolatey)) {
+        $script:chocolateyDir = Join-Path $env:SYSTEMDRIVE Chocolatey;
+    }
+    elseif (Test-Path (Join-Path ([Environment]::GetFolderPath("CommonApplicationData")) Chocolatey)) {
+        $script:chocolateyDir = Join-Path ([Environment]::GetFolderPath("CommonApplicationData")) Chocolatey;
+    }
+
+    Test-Path -Path $script:chocolateyDir;
+}
